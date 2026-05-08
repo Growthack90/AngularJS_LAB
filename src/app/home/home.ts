a4976903e7ea1e600a987a01c4997fe676793224
Growthack90 <hackmaker90@outlook.it>
2026-05-08 11:26:01 +0200

    add functionality in home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -7,7 +7,25 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     <section>
       <h2>Home</h2>
       <p>Questa e la pagina Home.</p>
+
+      <button (click)="counter = counter + 1">Incrementa contatore</button>
+      <p>Contatore: {{ counter }}</p>
+
+      <button (click)="cambiaColoreTesto()">Cambia colore testo</button>
+      <p [style.color]="coloreTesto">Testo con colore dinamico</p>
+
     </section>
   `
 })
-export class Home {}
+
+export class Home {
+
+  counter = 0;
+  
+  coloreTesto = 'blue';
+
+  cambiaColoreTesto() {
+    this.coloreTesto = this.coloreTesto === 'blue' ? 'green' : 'blue';
+  }
+
+}
